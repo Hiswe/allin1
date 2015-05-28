448286e120ff163057698bf6af2ad48c38b0ea1f
Hiswe <hiswehalya@gmail.com>
2015-05-28 17:17:32 +0700

    CONTENT – add translation and direction

--- locales/en.js
+++ locales/en.js
@@ -5,7 +5,7 @@
 	"contact": "contact",
 	"baseline": "As comfortable as a hotel<br /> but at a guesthouse price",
 	"contact-pres": "All in 1 Guesthouse is located right in the city center. It's a 5 minutes walk from Thapae Gate and the Sunday Market, a 10 minutes walk from the Night Bazaar. From the Airport, the Railway station or the Bus station, it will take around 10-15 minutes to get there.",
-	"contact-adress": "Adress:",
+	"contact-adress": "Adress",
 	"contact-phone": "Phone us:",
 	"contact-mobile": "Mobile phone:",
 	"contact-email": "Mail us:",
